7c87020c9518dd07979696dee172eb4c63105804
madraceee <raajanitheesh@gmail.com>
2024-01-03 23:14:49 +0530

    Added NavBar

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,17 +2,20 @@
 
 import CreateGame from "@/components/CreateGame"
 import PlayGame from "@/components/PlayGame"
+import NavBar from "@/components/NavBar"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ContractProvider from "@/hooks/contract"
-import store, { RootState } from "@/redux/store"
+import { RootState } from "@/redux/store"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from "@/redux/wallet/walletSlice"
 
 export default function Page() {
 
     const address = useSelector((state: RootState) => state.wallet.address);
     const router = useRouter();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (address === "") {
@@ -22,8 +25,9 @@ export default function Page() {
 
     return (
         <ContractProvider>
-            <div className="flex justify-center items-center min-h-screen">
-                <Tabs defaultValue="createGame" className="w-[400px] md:w-1/2 ">
+            <NavBar address={address} logout={() => dispatch(logout())} />
+            <div className="flex justify-center items-center mt-20">
+                <Tabs defaultValue="createGame" className=" min-w-fit w-[430px] md:w-1/2 ">
                     <TabsList className="grid w-full grid-cols-2">
                         <TabsTrigger value="createGame">Create Game</TabsTrigger>
                         <TabsTrigger value="playGame">Play Game</TabsTrigger>
